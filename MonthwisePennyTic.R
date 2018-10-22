5709204efad08eacdbc13444f5faa213a76f69cf
Circuited <paaras12@gmail.com>
2018-10-22 16:21:59 +0530

    MonthwisePennyTic modified to be readable

--- MonthwisePennyTic.R
+++ MonthwisePennyTic.R
@@ -16,29 +16,34 @@ idpennymonthbased <- function( filelocation,pennydef, yearinput){
   #finding unique companies
   uniquecompanies <- unique(taball$tic)
   uniquecompanies <- as.character(uniquecompanies)
-
+  
+  #Making a dataframe that comprises of these companies
   result <- data.frame(tic=character(),
                      prccmmean = numeric(), 
                      prclmmean = numeric(),
                      stringsAsFactors=FALSE)
   resultlength <- length(uniquecompanies)
-
+  
+  #For loop to loop over finding minniumum in the year. 
   for( i in 1:resultlength ){
+    
+    #Specific company talks about 1 company 
     specificcompany <- taball[taball$tic == uniquecompanies[i],]
+    
+    #Mean of stock prices in the year.
     prccmmean <- mean(specificcompany$prccm)
     prclmmean <- mean(specificcompany$prclm)
     result[i,1] <- uniquecompanies[i]
     result[i,2] <- prccmmean
     result[i,3] <- prclmmean
   }
-
-  listtic <- result[which(result$prclmmean <= 5),]
+  #You find whether the mean is below the benchmark price
+  listtic <- result[which(result$prclmmean <= pennydef),]
   listticlength1 <- nrow(listtic)
   listticlength2 <- min(table(taball$datadate))
   listtic[listticlength1+1,1] <- listticlength1
   listtic[listticlength1+1,2] <- listticlength2
   
   listtic
-  
 
 }
