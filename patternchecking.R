f0cb5736f41f452d5fcffc7dc1af1e16ea3d304b
Circuited <paaras12@gmail.com>
2019-01-15 03:06:39 +0530

    Cleaned the code, added more graphs, stats for analysis.

--- patternchecking.R
+++ patternchecking.R
@@ -21,8 +21,6 @@ uniquecompanies <- as.character(uniquecompanies)
 
 ###Plotting graph for each company 
 
-#Creating space for 3 graphs 
-par(mfrow=c(3,1))
 
 #Funtion to provide daily return 
 dailyreturn <- function(closeprice)
@@ -37,34 +35,58 @@ dailyreturn <- function(closeprice)
   result
 }
 
+#specificcompany <- taball[taball$tic == uniquecompanies[i],]
+#Get specific company list from Percentileranker.R
+
 #Initiate graph maker
 i = 0
-
 #Run Graphs!
-
 i <- i + 1
-#specificcompany <- taball[taball$tic == uniquecompanies[i],]
-
-#Get specific company list from Percentileranker.R
 
 specificcompany <- taball[taball$tic == specificcompanylist[i,1],]
 result <- dailyreturn(specificcompany$prccd)
- 
-plot(specificcompany$datadate, log(specificcompany$cshtrd+2),type ="b")
 
+#Creating space for 3 graphs 
+par(mfrow=c(3,1))
+
+plot(specificcompany$datadate, log(specificcompany$cshtrd+2),type ="b")
 #The 2 is added to log to avoid NA values
 plot(specificcompany$datadate, specificcompany$prccd)
 plot(specificcompany$datadate[-1], (result$V1), type ="b")
+length(which(result$V1 == 0))
 
-
+xmat = cbind((specificcompany$cshtrd+2),specificcompany$prccd,specificcompany$prchd,specificcompany$prcld,specificcompany$prcod);
+cr=cor(xmat)
+cr
+#With log trade volume
 xmat = cbind(log(specificcompany$cshtrd+2),specificcompany$prccd,specificcompany$prchd,specificcompany$prcld,specificcompany$prcod);
 cr=cor(xmat)
 cr
 
-summary(lm(log(specificcompany$cshtrd+2)~specificcompany$prccd))
+#correlation  with absolute value of results
+xmat = cbind(log(specificcompany$cshtrd[-1]+2),(result$V1));
+cr=cor(xmat)
+cr
 
 
-par(mfrow=c(2,1))
-plot(log(specificcompany$cshtrd+2), specificcompany$prccd)
+summary(lm((specificcompany$cshtrd+2)~specificcompany$prccd))
+#With log trade volume
+reg1<-summary(lm(log(specificcompany$cshtrd+2)~specificcompany$prccd))
+reg1
+
+reg2<-summary(lm(abs(result$V1)~log(specificcompany$cshtrd[-1]+2)))
+reg2
+
+reg3<-summary(lm((result$V1)~log(specificcompany$cshtrd[-1]+2)))
+reg3
+
+par(mfrow=c(1,1))
+
 plot(specificcompany$prccd,log(specificcompany$cshtrd+2))
+abline(reg1)
 
+plot(log(specificcompany$cshtrd[-1]+2),abs(result$V1))
+abline(reg2)
+
+plot(log(specificcompany$cshtrd[-1]+2),(result$V1))
+abline(reg3)
